41ef34909dccebc4928fd25ac18e588e428f7c8d
zf-jb <zachary.fowler@jetblue.com>
2024-03-11 16:35:35 -0400

    Added error catching code for variable winds.

--- app-server.py
+++ app-server.py
@@ -11,6 +11,9 @@ import selectors
 import traceback
 import libserver
 
+host = "127.0.0.1"
+port = 7890
+
 sel = selectors.DefaultSelector()
 
 
@@ -21,11 +24,6 @@ def accept_wrapper(sock):
     message = libserver.Message(sel, conn, addr)
     sel.register(conn, selectors.EVENT_READ, data=message)
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <host> <port>")
-    sys.exit(1)
-
-host, port = sys.argv[1], int(sys.argv[2])
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Avoid bind() exception: OSError: [Errno 48] Address already in use
 lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
